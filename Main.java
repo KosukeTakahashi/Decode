5ca7eb891c87bf04f44d0ac08ff49de9f02610fd
KosukeTakahashi <kousuke20010307@icloud.com>
2017-04-13 14:30:53 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,42 +1,33 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-    public static void main(String[] args) {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String userInput = "";
-        String temp;
-
-        System.out.print("Input the ciphers split by \':\'---");
-
-        try {
-            userInput = br.readLine();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        if ((!userInput.equals("")) && (!userInput.equals("exit"))) {
-            temp = Decry(userInput.split(":"));
-            System.out.println("result : " + temp);
-        } else if (userInput.equals("exit")) {
-            System.exit(0);
-        }
-
-        System.out.println("Enter \'exit\' to exit.");
-
-        main(args);
-    }
-
-    public static String Decry(String[] src) {
-        StringBuilder sb = new StringBuilder();
-        int temp;
-
-        for (String res : src) {
-            temp = Integer.valueOf(res, 16);
-            sb.append((char)temp);
-        }
-
-        return new String(sb);
-    }
+public class Main
+{
+	public static void main(String[] args)
+	{
+		String src = "髙橋孝輔";
+		int[] result = gen(src);
+		for (int r : result)
+		    System.out.println(Integer.toHexString(r));
+			
+		for (char c : decode(result))
+		    System.out.print(c);
+	}
+	
+	public static int[] gen(String s) {
+		char[] cs = s.toCharArray();
+		int[] res = new int[cs.length];
+		int idx = 0;
+		for (char c : cs)
+			res[idx++] = c;
+		return res;
+	}
+	
+	public static char[] decode(int[] src) {
+		char[] name = new char[src.length];
+		for (int i = 0; i < src.length; i++)
+			name[i] = (char)src[i];
+			
+		return name;
+	}
 }
